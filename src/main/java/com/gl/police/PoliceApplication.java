7cd76ce83df4a509efdef2b90af297cbf44c0045
ylf-github <18629450764@163.com>
2020-01-10 15:14:19 +0800

    添加DTO及daoAPI

--- src/main/java/com/gl/police/PoliceApplication.java
+++ src/main/java/com/gl/police/PoliceApplication.java
@@ -1,9 +1,13 @@
 package com.gl.police;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(
+        "com.gl.police.daoAPI"
+)
 public class PoliceApplication {
 
     public static void main(String[] args) {
