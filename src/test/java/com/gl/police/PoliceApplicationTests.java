67bd3660b2f4f7a57b90b0b49bf20970bf2dee41
ylf-github <18629450764@163.com>
2020-01-10 13:55:33 +0800

    init

---
+++ src/test/java/com/gl/police/PoliceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.police;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
