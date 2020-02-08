d75f317b09a1fb539dcacb474b16eddcbaac9cce
ylf-github <18629450764@163.com>
2020-02-08 20:57:16 +0800

    添加token拦截器

--- src/main/java/com/gl/police/config/TokenAuthConfig.java
+++ src/main/java/com/gl/police/config/TokenAuthConfig.java
@@ -1,39 +1,37 @@
-//package com.gl.police.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import java.util.ArrayList;
-//
-//
-///**
-// * @author: leifeng.ye
-// * @date: 2020-01-02
-// * @desc:
-// */
-//@Component
-//public class TokenAuthConfig  implements WebMvcConfigurer {
-//
-//    @Autowired
-//    private TokenAuth tokenAuth;
-//
-//    //注册拦截器
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        ArrayList<String> list=new ArrayList<>();
-//        list.add("/api/v1/login");
-//        list.add("/api/v1/userLogin");
-//        list.add("/api/v1/loginByPhone");
-//        list.add("/api/v1/authByPhoneCode");
-//        list.add("/index.html");
-//        list.add("/css/**");
-//        list.add("/js/**");
-//        list.add("/img/**");
-//        list.add("/fonts/**");
-//        list.add("/api/v1/selectUserSignAttendPlanList");
-//        list.add("/");
-//        registry.addInterceptor(tokenAuth).addPathPatterns("/**").excludePathPatterns(list);
-//    }
-//}
+package com.gl.police.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+
+
+/**
+ * @author: leifeng.ye
+ * @date: 2020-01-02
+ * @desc:
+ */
+@Component
+public class TokenAuthConfig  implements WebMvcConfigurer {
+
+    @Autowired
+    private TokenAuth tokenAuth;
+
+    //注册拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        ArrayList<String> list=new ArrayList<>();
+        list.add("/api/v1/login");
+        list.add("/api/v1/loginByPhone");
+        list.add("/api/v1/authByPhoneCode");
+        list.add("/index.html");
+        list.add("/css/**");
+        list.add("/js/**");
+        list.add("/img/**");
+        list.add("/fonts/**");
+        list.add("/");
+        registry.addInterceptor(tokenAuth).addPathPatterns("/**").excludePathPatterns(list);
+    }
+}
